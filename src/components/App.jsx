1eeb9d5523b65ac4301fc7514ae727faa5fa822c
Kamil Kucharczyk <kamil.kucharczyk007@gmail.com>
2025-11-23 21:51:35 +0100

    Components draft correction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 // import { FeedbackOptions } from './project 2/feedbackOptions/FeedbackOptions';
 
-class App extends Component {
-  // state = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0
-  // }
-  // countTotalFeedback = () => {
-  // const {good, neutral, bad} = this.state;
-  // return good + neutral + bad;
-  // }
-  // countPositiveFeedbackPercentage = () => {
-  //   const {good, neutral, bad} = this.state;
-  //   const total = good + neutral + bad;
-  //   if (total === 0) {
-  //     return 0
-  //   } else {
-  //     return Math.random((good/total)*100);
-  //   }
-  // }
-  //   render() {}
-}
+// class App extends Component {
+// state = {
+//   good: 0,
+//   neutral: 0,
+//   bad: 0
+// }
+// countTotalFeedback = () => {
+// const {good, neutral, bad} = this.state;
+// return good + neutral + bad;
+// }
+// countPositiveFeedbackPercentage = () => {
+//   const {good, neutral, bad} = this.state;
+//   const total = good + neutral + bad;
+//   if (total === 0) {
+//     return 0
+//   } else {
+//     return Math.random((good/total)*100);
+//   }
+// }
+//   render() {}
