871118d4149ba91237ea38020360c28aa05f2c4f
Kamil Kucharczyk <kamil.kucharczyk007@gmail.com>
2026-01-05 17:45:01 +0100

    Local storage update

--- src/components/project2.1/App.jsx
+++ src/components/project2.1/App.jsx
@@ -18,6 +18,19 @@ export class App extends Component {
     number: '',
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = ({ name, number }) => {
     const listedContact = this.state.contacts.some(
       contact => contact.name.toLowerCase() === name.toLowerCase()
