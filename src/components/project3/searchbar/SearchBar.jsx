931ef8eff6d8157ed8962de5b3558fb9bafee098
Kamil Kucharczyk <kamil.kucharczyk007@gmail.com>
2026-01-06 20:10:27 +0100

    Searchbar component created

---
+++ src/components/project3/searchbar/SearchBar.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from './searchBar.module.css';
+
+class SearchBar extends Component {
+  state = {
+    inputValue: '',
+  };
+
+  handleChange = event => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.inputValue);
+    this.setState({ inputValue: '' });
+  };
+
+  render() {
+    return (
+      <header className={css.searchBar}>
+        <form className={css.searchForm} onSubmit={this.handleSubmit}>
+          <button type="submit" className={css.searchForm_button}>
+            <span className={css.searchForm_button_label}>Search</span>
+          </button>
+          <input
+            className={css.searchForm_input}
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+            onChange={this.handleChange}
+            value={this.state.inputValue}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
