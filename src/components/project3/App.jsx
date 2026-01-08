07e2928b47a42b5e8482937077e0e670db8316c4
Kamil Kucharczyk <kamil.kucharczyk007@gmail.com>
2026-01-08 19:03:07 +0100

    Image gallery and item created

--- src/components/project3/App.jsx
+++ src/components/project3/App.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import SearchBar from './searchbar/SearchBar';
+import axios from 'axios';
+
+const baseUrl = 'https://pixabay.com/api/';
 
 class App extends Component {
   render() {
